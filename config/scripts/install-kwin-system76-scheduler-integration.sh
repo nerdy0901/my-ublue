c3a56fae84c4ec5c12a9dbebef53357b84e4c2d8
nerdy0901 <150199648+nerdy0901@users.noreply.github.com>
2023-11-20 04:42:02 +0700

    fix

--- config/scripts/install-kwin-system76-scheduler-integration.sh
+++ config/scripts/install-kwin-system76-scheduler-integration.sh
@@ -11,3 +11,4 @@ echo 'Install KWin Script for system76-scheduler Integration'
 git clone https://github.com/maxiberta/kwin-system76-scheduler-integration.git --depth 1 /tmp/kwin-system76-scheduler-integration
 kpackagetool5 --type=KWin/Script --global --install /tmp/kwin-system76-scheduler-integration
 rm -rf /tmp/kwin-system76-scheduler-integration
+systemctl --global enable com.system76.Scheduler.dbusproxy.service
